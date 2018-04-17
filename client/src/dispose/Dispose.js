ea354955fd5fec313e55856256f3197e6c8841b5
maslovmichail20 <maslov.mikhail2000@gmail.com>
2018-04-17 23:04:31 +0300

    Change style

--- client/src/dispose/Dispose.js
+++ client/src/dispose/Dispose.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import './dispose.css';
 
 import Field from '../field/Field.js';
 import Navy from '../navy/Navy.js';
@@ -9,7 +10,7 @@ import {DragDropContext} from 'react-dnd';
 class Dispose extends Component {
   render() {
     return (
-      <div>
+      <div className={'dispose'}>
         <Field matrix={this.props.matrix}
                changeMatrixHandler={this.props.changeMatrixHandler}/>
         <Navy />
